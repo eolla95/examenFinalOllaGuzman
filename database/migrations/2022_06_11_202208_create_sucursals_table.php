f2de3aa67d167f2e45d7d3676b7899fd6f73e0b1
Kelvin Guzman <kelvin.guzman@oj.gob.sv>
2022-06-11 15:00:38 -0600

    correcion

---
+++ database/migrations/2022_06_11_202208_create_sucursals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSucursalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sucursals', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->text('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sucursals');
+    }
+}
