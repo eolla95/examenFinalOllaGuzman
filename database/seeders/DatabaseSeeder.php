49456908f2697d3fd9c3f3616decfb602ac49d8f
Kelvin Guzman <kelvin.guzman@oj.gob.sv>
2022-06-11 15:31:53 -0600

    usuario admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(2)->create();
+        \App\Models\User::factory(1)->create();
         \App\Models\Sucursal::factory(10)->create();
         \App\Models\Empleado::factory(10)->create();
         
